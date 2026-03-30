8f06cb72a64fac27357239d4343a59f36f09610a
johnclecio <johncleciofl@gmail.com>
2026-03-30 20:15:14 -0300

    add docker 2.5

--- telegram-bot/src/main/java/com/johnlima/telegrambot/config/TelegramInitializer.java
+++ telegram-bot/src/main/java/com/johnlima/telegrambot/config/TelegramInitializer.java
@@ -1,28 +1,19 @@
 package com.johnlima.telegrambot.config;
 
-import com.johnlima.telegrambot.controler.TelegramBot;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import com.johnlima.telegrambot.controler.TelegramBot;
 
-
-
-
-
-@Configuration
+@Component
 public class TelegramInitializer {
 
-    @Autowired
-    private TelegramBot bot;
-
-    @PostConstruct
-    public void init() {
+    public TelegramInitializer(TelegramBot bot) {
         try {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(bot);
+
             System.out.println("🤖 Bot iniciado com sucesso!");
         } catch (Exception e) {
             e.printStackTrace();
