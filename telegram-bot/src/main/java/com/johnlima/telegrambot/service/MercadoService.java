79b736f0b1f86411c489bc084eb3633ffa4f306f
johnclecio <johncleciofl@gmail.com>
2026-03-30 22:10:28 -0300

    retirando  ibovespa 1.5

--- telegram-bot/src/main/java/com/johnlima/telegrambot/service/MercadoService.java
+++ telegram-bot/src/main/java/com/johnlima/telegrambot/service/MercadoService.java
@@ -1,36 +1,20 @@
 package com.johnlima.telegrambot.service;
 
-import com.johnlima.telegrambot.client.CryptoClient;
-import com.johnlima.telegrambot.client.IbovespaClient;
 import com.johnlima.telegrambot.client.NewsClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MercadoService {
 
-    private final CryptoClient cryptoClient;
-    private final IbovespaClient ibovespaClient;
+
     private final NewsClient newsClient;
 
-    public MercadoService(CryptoClient cryptoClient, IbovespaClient ibovespaClient, NewsClient newsClient) {
-        this.cryptoClient = cryptoClient;
-        this.ibovespaClient = ibovespaClient;
+    public MercadoService( NewsClient newsClient) {
+
         this.newsClient = newsClient;
     }
 
-    public String getPrecoBitcoin() {
-        double valor = cryptoClient.buscarPrecoBitcoin();
 
-        if (valor == 0) {
-            return "Erro ao buscar Bitcoin 😢";
-        }
-
-        return String.format("💰 Bitcoin agora: R$ %.2f", valor);
-    }
-
-    public String getIbovespa() {
-        return ibovespaClient.buscarIbovespa();
-    }
 
 
 
